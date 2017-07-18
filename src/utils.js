e7326ece9f588c706f770ae5bc52a10140f6df07
Pasca Ovidiu Ionut <pascaionut94@yahoo.com>
2017-07-19 00:11:44 +0300

    Small fix

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ module.exports = {
 
 function createHashFromString(string) {
     if (Array.prototype.reduce) {
-        return this.split("").reduce(function (a, b) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0)
+        return string.split("").reduce(function (a, b) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0)
     }
     var hash = 0;
     if (string.length === 0) {
